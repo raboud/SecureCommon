4e12601ef0032943c6e4dd812e237a490cf0d23e
Robert Raboud <robert.raboud@randreng.com>
2019-05-07 21:02:25 -0400

    More restructuring

---
+++ app/Components/header/header.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Router } from '@angular/router';
+import { AuthService } from 'src/common/app/Services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  public isActive = true;
+  public isAdmin = false;
+  public authenticated = false;
+  private subscription: Subscription;
+  public userName = '';
+  badge = 0;
+
+  @Input() product: string;
+
+  constructor(
+    private router: Router,
+    private service: AuthService) { }
+
+  ngOnInit() {
+    this.subscription = this.service.authentication$.subscribe(res => {
+      this.authenticated = res;
+      this.isAdmin = this.service.IsAdmin;
+      this.userName = this.service.UserData ? this.service.UserData.Name : '';
+    });
+
+    if (window.location.hash) {
+        this.service.AuthorizedCallback();
+      }
+
+    this.authenticated = this.service.IsAuthorized;
+
+    if (this.authenticated) {
+        this.isAdmin = this.service.IsAdmin;
+        if (this.service.UserData) {
+              this.userName = this.service.UserData.Name;
+          }
+      }
+    }
+
+    menu() {
+      this.isActive = !this.isActive;
+    }
+
+    logout() {
+      this.service.Signoff();
+
+    }
+
+    login() {
+      this.service.Authorize();
+    }
+
+
+}
